a02394751d9097939c4779dc5764083b7d707326
Jelco <53396500+Jelcoo@users.noreply.github.com>
2022-11-14 09:19:04 +0100

    bot

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
 basic.forever(function () {
-	
+    if (DFRobotMaqueenPlus.readPatrol(Patrol.L1) === 1 && DFRobotMaqueenPlus.readPatrol(Patrol.R1) === 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 255)
+    } else {
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    }
 })
