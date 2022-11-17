d326f4a25d6ee9bd5cf4570ef20c235b4ce0ce7a
Jelco <53396500+Jelcoo@users.noreply.github.com>
2022-11-17 16:36:07 +0100

    Update to blocks

--- main.ts
+++ main.ts
@@ -5,39 +5,34 @@ let L3 = 0
 let R1 = 0
 let R2 = 0
 let R3 = 0
-
+input.onButtonPressed(Button.A, function () {
+    running = !(running)
+})
 basic.forever(function () {
-    if (!running) {
+    if (!(running)) {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
         return
     }
-
     L1 = DFRobotMaqueenPlus.readPatrol(Patrol.L1)
     L2 = DFRobotMaqueenPlus.readPatrol(Patrol.L2)
     L3 = DFRobotMaqueenPlus.readPatrol(Patrol.L3)
     R1 = DFRobotMaqueenPlus.readPatrol(Patrol.R1)
     R2 = DFRobotMaqueenPlus.readPatrol(Patrol.R2)
     R3 = DFRobotMaqueenPlus.readPatrol(Patrol.R3)
-
-    if (!R1 && L1 && L2) {
+    if (!(R1) && L1 && L2) {
         DFRobotMaqueenPlus.mototStop(Motors.M1)
         DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 100)
-    } else if (R1 && R2 && !L1) {
+    } else if (R1 && R2 && !(L1)) {
         DFRobotMaqueenPlus.mototStop(Motors.M2)
         DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 100)
-    } else if (R1 && L1 && !R2 && !L2) {
+    } else if (R1 && L1 && !(R2) && !(L2)) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
     }
 })
-
 basic.forever(function () {
     if (running) {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.GREEN)
     } else {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.RED)
     }
-})
-
-input.onButtonPressed(Button.A, function () {
-    running = !running
 })
