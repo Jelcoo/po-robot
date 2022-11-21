d70162dc96ba16cf2c5195fc7805853ab22fbf3c
Jelco <53396500+Jelcoo@users.noreply.github.com>
2022-11-21 09:15:12 +0100

    Speed & stop on gray

--- main.ts
+++ main.ts
@@ -5,28 +5,36 @@ let L3 = 0
 let R1 = 0
 let R2 = 0
 let R3 = 0
+let L1Gray = 0
+let R1Gray = 0
 input.onButtonPressed(Button.A, function () {
     running = !(running)
 })
 basic.forever(function () {
-    if (!(running)) {
-        DFRobotMaqueenPlus.mototStop(Motors.ALL)
-        return
-    }
     L1 = DFRobotMaqueenPlus.readPatrol(Patrol.L1)
     L2 = DFRobotMaqueenPlus.readPatrol(Patrol.L2)
     L3 = DFRobotMaqueenPlus.readPatrol(Patrol.L3)
     R1 = DFRobotMaqueenPlus.readPatrol(Patrol.R1)
     R2 = DFRobotMaqueenPlus.readPatrol(Patrol.R2)
     R3 = DFRobotMaqueenPlus.readPatrol(Patrol.R3)
-    if (!(R1) && L1 && L2) {
+    L1Gray = DFRobotMaqueenPlus.readPatrolVoltage(Patrol.L1)
+    R1Gray = DFRobotMaqueenPlus.readPatrolVoltage(Patrol.R1)
+
+    if (!(running)) {
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        return
+    }
+
+    if (L1Gray > 3000 && L1Gray < 3600 && R1Gray > 3000 && R1Gray < 3600) {
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    } else if (!(R1) && L1 && L2) {
         DFRobotMaqueenPlus.mototStop(Motors.M1)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 100)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 125)
     } else if (R1 && R2 && !(L1)) {
         DFRobotMaqueenPlus.mototStop(Motors.M2)
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 100)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 125)
     } else if (R1 && L1 && !(R2) && !(L2)) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 200)
     }
 })
 basic.forever(function () {
