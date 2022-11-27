d78a8fe6c59408d43f5ff7f74bcfda62a83b193d
Jelco <53396500+Jelcoo@users.noreply.github.com>
2022-11-27 22:11:57 +0100

    Some radio and shit

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let running = false
+let searching = false
 let L1 = 0
 let L2 = 0
 let L3 = 0
@@ -8,7 +9,16 @@ let R3 = 0
 let L1Gray = 0
 let R1Gray = 0
 input.onButtonPressed(Button.A, function () {
-    running = !(running)
+    running = false
+    searching = false
+})
+input.onButtonPressed(Button.B, function () {
+    running = true
+    searching = false
+})
+input.onButtonPressed(Button.AB, function () {
+    running = true
+    searching = true
 })
 basic.forever(function () {
     L1 = DFRobotMaqueenPlus.readPatrol(Patrol.L1)
@@ -23,25 +33,37 @@ basic.forever(function () {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
         return
     }
-    if (R3 && !(L3) && (L1 || L2 || R1 || R2)) {
-        basic.pause(300)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 75)
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 75)
-        basic.pause(400)
-    } else if (!(R1) && L1 && L2) {
-        DFRobotMaqueenPlus.mototStop(Motors.M1)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 125)
-    } else if (R1 && R2 && !(L1)) {
-        DFRobotMaqueenPlus.mototStop(Motors.M2)
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 125)
-    } else if (R1 && L1 && !(R2) && !(L2)) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 200)
+    if (searching) {
+        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+    } else {
+        if (R3 && L3) {
+            radio.sendNumber(1)
+        }
+        if (R3 && !(L3)) {
+            radio.sendNumber(0)
+            radio.sendNumber(2)
+            basic.pause(300)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 75)
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 75)
+            basic.pause(400)
+        } else if (!(R1) && L1 && L2) {
+            DFRobotMaqueenPlus.mototStop(Motors.M1)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+        } else if (R1 && R2 && !(L1)) {
+            DFRobotMaqueenPlus.mototStop(Motors.M2)
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
+        } else if (R1 && L1 && !(R2) && !(L2)) {
+            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
+        }
     }
 })
 basic.forever(function () {
-    if (running) {
+    if (searching) {
+        DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.BLUE)
+    } else if (running) {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.GREEN)
     } else {
         DFRobotMaqueenPlus.setRGBLight(RGBLight.RGBA, Color.RED)
     }
 })
+radio.setFrequencyBand(88)
