fa589b97958d9cb760836d89de7bf01089d5f3e7
Jelco <53396500+Jelcoo@users.noreply.github.com>
2022-11-28 10:10:34 +0100

    Fixes

--- main.ts
+++ main.ts
@@ -8,6 +8,8 @@ let R2 = 0
 let R3 = 0
 let L1Gray = 0
 let R1Gray = 0
+let R3Gray = 0
+let L3Gray = 0
 input.onButtonPressed(Button.A, function () {
     running = false
     searching = false
@@ -27,11 +29,16 @@ basic.forever(function () {
     R1 = DFRobotMaqueenPlus.readPatrol(Patrol.R1)
     R2 = DFRobotMaqueenPlus.readPatrol(Patrol.R2)
     R3 = DFRobotMaqueenPlus.readPatrol(Patrol.R3)
-    L1Gray = DFRobotMaqueenPlus.readPatrolVoltage(Patrol.L1)
     R1Gray = DFRobotMaqueenPlus.readPatrolVoltage(Patrol.R1)
+    L1Gray = DFRobotMaqueenPlus.readPatrolVoltage(Patrol.L1)
+    R3Gray = DFRobotMaqueenPlus.readPatrolVoltage(Patrol.R3)
+    L3Gray = DFRobotMaqueenPlus.readPatrolVoltage(Patrol.L3)
     if (!(running)) {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
         return
+    }
+    if (R1Gray > 3000 && L1Gray > 3000 && R3Gray < 3000) {
+        searching = true
     }
     if (searching) {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
@@ -39,19 +46,19 @@ basic.forever(function () {
         if (R3 && L3) {
             radio.sendNumber(1)
         }
-        if (R3 && !(L3)) {
+        if (R3 && !(L3) && !(R2 || L2)) {
             radio.sendNumber(0)
             radio.sendNumber(2)
             basic.pause(300)
             DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 75)
             DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 75)
-            basic.pause(400)
+            basic.pause(500)
         } else if (!(R1) && L1 && L2) {
             DFRobotMaqueenPlus.mototStop(Motors.M1)
-            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+            DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 60)
         } else if (R1 && R2 && !(L1)) {
             DFRobotMaqueenPlus.mototStop(Motors.M2)
-            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
+            DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 60)
         } else if (R1 && L1 && !(R2) && !(L2)) {
             DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
         }
